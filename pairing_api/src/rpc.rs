30960738705c508bf3c5cd5ef410e9a7d17c3f50
Samuel Onoja <samiodev@icloud.com>
2024-08-30 20:11:14 +0100

    save dev state

--- pairing_api/src/rpc.rs
+++ pairing_api/src/rpc.rs
@@ -1,59 +1,16 @@
-mod methods;
+mod params;
+#[macro_use]
 mod shared;
 
 use {
-    methods::{
+    params::{
         pairing_delete::PairingDeleteRequest,
         pairing_extend::PairingExtendRequest,
         pairing_ping::PairingPingRequest,
     },
     paste::paste,
     serde::{Deserialize, Serialize},
-    shared::{IrnMetadata, RelayProtocolHelpers, RelayProtocolMetadata},
 };
-
-// Convenience macro to de-duplicate implementation for different parameter
-// sets.
-macro_rules! impl_relay_protocol_metadata {
-    ($param_type:ty,$meta:ident) => {
-        paste! {
-            impl RelayProtocolMetadata for $param_type {
-                fn irn_metadata(&self) -> IrnMetadata {
-                    match self {
-                        [<$param_type>]::PairingDelete(_) => methods::pairing_delete::[<IRN_ $meta:upper _METADATA>],
-                        [<$param_type>]::PairingExtend(_) => methods::pairing_extend::[<IRN_ $meta:upper _METADATA>],
-                        [<$param_type>]::PairingPing(_) => methods::pairing_ping::[<IRN_ $meta:upper _METADATA>],
-                    }
-                }
-            }
-        }
-    }
-}
-
-macro_rules! impl_relay_protocol_helpers {
-    ($param_type:ty) => {
-        paste! {
-            impl RelayProtocolHelpers for $param_type {
-                type Params = Self;
-
-                fn irn_try_from_tag(value: Value, tag: u32) -> Result<Self::Params, ParamsError> {
-                    match tag {
-                        tag if tag == methods::pairing_delete::IRN_RESPONSE_METADATA.tag => {
-                            Ok(Self::PairingDelete(serde_json::from_value(value)?))
-                        }
-                        tag if tag == methods::pairing_extend::IRN_RESPONSE_METADATA.tag => {
-                            Ok(Self::PairingExtend(serde_json::from_value(value)?))
-                        }
-                        tag if tag == methods::pairing_ping::IRN_RESPONSE_METADATA.tag => {
-                            Ok(Self::PairingPing(serde_json::from_value(value)?))
-                        }
-                        _ => Err(ParamsError::ResponseTag(tag)),
-                    }
-                }
-            }
-        }
-    };
-}
 
 /// Pairing API request parameters.
 ///
@@ -63,11 +20,11 @@ macro_rules! impl_relay_protocol_helpers {
 #[serde(tag = "method", content = "params")]
 pub enum PairingRequestParams {
     #[serde(rename = "wc_pairingExtend")]
-    PairingExtend(PairingExtendRequest),
+    Extend(PairingExtendRequest),
     #[serde(rename = "wc_pairingDelete")]
-    PairingDelete(PairingDeleteRequest),
+    Delete(PairingDeleteRequest),
     #[serde(rename = "wc_pairingPing")]
-    PairingPing(PairingPingRequest),
+    Ping(PairingPingRequest),
 }
 
 impl_relay_protocol_metadata!(PairingRequestParams, request);
--- pairing_api/src/rpc/shared.rs
+++ pairing_api/src/rpc/shared.rs
@@ -1,4 +1,10 @@
-use serde_json::Value;
+use {
+    crate::rpc::params,
+    paste::paste,
+    relay_rpc::rpc::ErrorData,
+    serde::{Deserialize, Serialize},
+    serde_json::Value,
+};
 
 /// Relay IRN protocol metadata.
 ///
@@ -45,3 +51,62 @@ pub enum ParamsError {
     #[error("Response tag={0} does not match any of the Sign API methods")]
     ResponseTag(u32),
 }
+
+// Convenience macro to de-duplicate implementation for different parameter
+// sets.
+#[macro_export]
+macro_rules! impl_relay_protocol_metadata {
+    ($param_type:ty,$meta:ident) => {
+        paste! {
+            impl $crate::rpc::shared::RelayProtocolMetadata for $param_type {
+                fn irn_metadata(&self) -> $crate::rpc::shared::IrnMetadata {
+                    match self {
+                        [<$param_type>]::Delete(_) => params::pairing_delete::[<IRN_ $meta:upper _METADATA>],
+                        [<$param_type>]::Extend(_) => params::pairing_extend::[<IRN_ $meta:upper _METADATA>],
+                        [<$param_type>]::Ping(_) => params::pairing_ping::[<IRN_ $meta:upper _METADATA>],
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Convenience macro to de-duplicate implementation for different parameter
+// sets.
+#[macro_export]
+macro_rules! impl_relay_protocol_helpers {
+    ($param_type:ty) => {
+        paste! {
+            impl $crate::rpc::shared::RelayProtocolHelpers for $param_type {
+                type Params = Self;
+
+                fn irn_try_from_tag(value: Value, tag: u32) -> Result<Self::Params, ParamsError> {
+                    match tag {
+                        tag if tag == params::pairing_delete::IRN_RESPONSE_METADATA.tag => {
+                            Ok(Self::Delete(serde_json::from_value(value)?))
+                        }
+                        tag if tag == params::pairing_extend::IRN_RESPONSE_METADATA.tag => {
+                            Ok(Self::Extend(serde_json::from_value(value)?))
+                        }
+                        tag if tag == params::pairing_ping::IRN_RESPONSE_METADATA.tag => {
+                            Ok(Self::Ping(serde_json::from_value(value)?))
+                        }
+                        _ => Err(ParamsError::ResponseTag(tag)),
+                    }
+                }
+            }
+        }
+    };
+}
+
+/// Typed error response parameters.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ResponseParamsError {
+    Delete(ErrorData),
+    Extend(ErrorData),
+    Ping(ErrorData),
+}
+
+impl_relay_protocol_metadata!(ResponseParamsError, response);
+impl_relay_protocol_helpers!(ResponseParamsError);
