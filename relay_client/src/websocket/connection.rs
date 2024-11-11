4e094377a0fd51d3cba09cf3adba17beeb088416
Samuel Onoja <samiodev@icloud.com>
2024-11-11 14:35:02 +0100

    debug ws socket dirty close

--- relay_client/src/websocket/connection.rs
+++ relay_client/src/websocket/connection.rs
@@ -81,6 +81,15 @@ pub async fn connection_event_loop<T>(
                     }
 
                     StreamEvent::InboundError(error) => {
+                        if let ClientError::WebsocketClient(WebsocketClientError::Transport(err)) = &error {
+                            if err.to_string().contains("Operation timed out") {
+                                conn.reset();
+                            };
+
+                            if err.to_string().contains("unexpected end of file") {
+                                conn.reset();
+                            }
+                        }
                         handler.inbound_error(error);
                     }
 
@@ -123,7 +132,6 @@ impl Connection {
 
         match stream {
             Some(mut stream) => stream.close(None).await,
-
             None => Err(WebsocketClientError::ClosingFailed(TransportError::AlreadyClosed).into()),
         }
     }
