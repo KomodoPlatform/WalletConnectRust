edd96c9ddcb60b7b19809a3297e1a1ea1d3dbb10
Samuel Onoja <samiodev@icloud.com>
2024-11-12 08:29:48 +0100

    remove debug log

--- relay_client/src/websocket/stream.rs
+++ relay_client/src/websocket/stream.rs
@@ -229,17 +229,13 @@ impl ClientStream {
         loop {
             // `poll_ready() needs to be called before each `start_send()` to make sure the
             // sink is ready to accept more data.
-            println!("poll ready");
             match self.socket.poll_ready_unpin(cx) {
                 // The sink is ready to accept more data.
                 Poll::Ready(Ok(())) => {
                     if let Poll::Ready(Some(next_message)) = self.outbound_rx.poll_recv(cx) {
-                        println!("start send");
                         if let Err(err) = self.socket.start_send_unpin(next_message) {
-                            println!("start send error {err:?}");
                             return Poll::Ready(Err(err));
                         }
-                        println!("after start send");
 
                         should_flush = true;
                     } else if should_flush {
